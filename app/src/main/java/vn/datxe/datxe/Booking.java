1a4986ce8aedfaa02d73df7fdca8e368f25fc41e
Le Tan Vu <imleeu@gmail.com>
2016-03-28 23:12:15 +0700

    update with socket io code

--- app/src/main/java/vn/datxe/datxe/Booking.java
+++ app/src/main/java/vn/datxe/datxe/Booking.java
@@ -1,13 +1,66 @@
 package vn.datxe.datxe;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
 
 public class Booking extends AppCompatActivity {
-
+    private Socket socket;
+    {
+        try {
+            socket = IO.socket("https://datxe.herokuapp.com");
+        } catch (URISyntaxException e) {}
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking);
+        socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+
+            @Override
+            public void call(Object... args) {
+                socket.emit("foo", "hi");
+            }
+
+        }).on("event", new Emitter.Listener() {
+
+            @Override
+            public void call(Object... args) {}
+
+        }).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+
+            @Override
+            public void call(Object... args) {}
+
+        });
+        socket.connect();
+    }
+
+    public void selectSeat(View view){
+        int buttonId = view.getId();
+
+    }
+
+    public void connect2Server(){
+
+    }
+
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        socket.disconnect();
     }
 }
